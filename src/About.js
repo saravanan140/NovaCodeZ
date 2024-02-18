9ad7970c0a40234c8df180258eb3e52b94425385
saravanan140 <149892951+saravanan140@users.noreply.github.com>
2024-02-18 10:19:21 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -97,8 +97,8 @@ const About = () => {
                 <div class="row g-5 align-items-center">
                     <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                         <p class="section-title text-secondary">About Us<span></span></p>
-                        <h1 className="mb-5">#1 Digital solution with 1+ years of experience</h1>
-                <p className="mb-4">Welcome to <b>NovaCodeZ</b>, where innovation meets experience to redefine digital excellence. As a pioneering force in the industry, we have proudly held the position as the #1 digital solution provider for over 1+ years.</p>
+                        <h1 className="mb-5">#1 Digital solution with 3+ years of experience</h1>
+                <p className="mb-4">Welcome to <b>NovaCodeZ</b>, where innovation meets experience to redefine digital excellence. As a pioneering force in the industry, we have proudly held the position as the #1 digital solution provider for over 3+ years.</p>
                 <div className="skill mb-4">
                     <div className="d-flex justify-content-between">
                         <p className="mb-2">Full stack development</p>
@@ -150,22 +150,22 @@ const About = () => {
                 <div class="row g-4">
                     <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
                         <i class="fa fa-certificate fa-3x text-secondary mb-3"></i>
-                        <h1 class="text-white mb-2" data-toggle="counter-up">1+</h1>
+                        <h1 class="text-white mb-2" data-toggle="counter-up">3+</h1>
                         <p class="text-white mb-0">Years Experience</p>
                     </div>
                     <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
                         <i class="fa fa-users-cog fa-3x text-secondary mb-3"></i>
-                        <h1 class="text-white mb-2" data-toggle="counter-up">15</h1>
+                        <h1 class="text-white mb-2" data-toggle="counter-up">25</h1>
                         <p class="text-white mb-0">Team Members</p>
                     </div>
                     <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
                         <i class="fa fa-users fa-3x text-secondary mb-3"></i>
-                        <h1 class="text-white mb-2" data-toggle="counter-up">20</h1>
+                        <h1 class="text-white mb-2" data-toggle="counter-up">60+</h1>
                         <p class="text-white mb-0">Satisfied Clients</p>
                     </div>
                     <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
                         <i class="fa fa-check fa-3x text-secondary mb-3"></i>
-                        <h1 class="text-white mb-2" data-toggle="counter-up">14</h1>
+                        <h1 class="text-white mb-2" data-toggle="counter-up">100+</h1>
                         <p class="text-white mb-0">Compleate Projects</p>
                     </div>
                 </div>
