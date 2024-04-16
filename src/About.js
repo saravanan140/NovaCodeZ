819b7b9bff0626f614d15e00257036a7a75b31d8
saravanan140 <149892951+saravanan140@users.noreply.github.com>
2024-04-16 12:09:16 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -97,8 +97,8 @@ const About = () => {
                 <div class="row g-5 align-items-center">
                     <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                         <p class="section-title text-secondary">About Us<span></span></p>
-                        <h1 className="mb-5">#1 Digital solution with 3+ years of experience</h1>
-                <p className="mb-4">Welcome to <b>NovaCodeZ</b>, where innovation meets experience to redefine digital excellence. As a pioneering force in the industry, we have proudly held the position as the #1 digital solution provider for over 3+ years.</p>
+                        <h1 className="mb-5">#1 Digital solution with 6+ years of experience</h1>
+                <p className="mb-4">Welcome to <b>NovaCodeZ</b>, where innovation meets experience to redefine digital excellence. As a pioneering force in the industry, we have proudly held the position as the #1 digital solution provider for over 6+ years.</p>
                 <div className="skill mb-4">
                     <div className="d-flex justify-content-between">
                         <p className="mb-2">Full stack development</p>
@@ -150,7 +150,7 @@ const About = () => {
                 <div class="row g-4">
                     <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
                         <i class="fa fa-certificate fa-3x text-secondary mb-3"></i>
-                        <h1 class="text-white mb-2" data-toggle="counter-up">3+</h1>
+                        <h1 class="text-white mb-2" data-toggle="counter-up">6+</h1>
                         <p class="text-white mb-0">Years Experience</p>
                     </div>
                     <div class="col-md-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
